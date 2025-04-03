65884a2907c7837de7197b5c2d5fd0b5a7fd6198
vae <18137693952@163.com>
2025-04-03 11:02:10 +0800

    feat: 生成文件配置更新

--- rslib.config.ts
+++ rslib.config.ts
@@ -8,4 +8,24 @@ export default defineConfig({
       dts: true,
     },
   ],
+  output: {
+    minify: {
+      js: true, // 启用 JavaScript 压缩
+      jsOptions: {
+        minimizerOptions: {
+          mangle: true, // 启用变量和函数名的混淆（压缩）
+          minify: true, // 启用进一步的最小化（压缩）
+          compress: {
+            defaults: true, // 启用默认压缩选项
+            unused: true, // 删除未使用的代码
+            dead_code: true, // 删除死代码
+            toplevel: true, // 删除顶层无用代码
+          },
+          format: {
+            comments: false, // 移除所有注释
+          },
+        },
+      },
+    },
+  },
 });
