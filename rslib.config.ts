fbdcfaa7be023438f387f058502993ff01db2286
vae <18137693952@163.com>
2025-04-03 11:09:12 +0800

    feat: 输出产物改为 cjs

--- rslib.config.ts
+++ rslib.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   lib: [
     {
-      format: 'esm',
+      format: 'cjs',
       syntax: 'es2021',
       dts: true,
     },
